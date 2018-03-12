17fb726c869d41b57c4339569624bd6052f473b7
Márcio Gabriel <marcioggs@gmail.com>
2018-03-11 22:04:29 -0300

    Added Twitter icon with link.

--- src/components/ArtistRow.js
+++ src/components/ArtistRow.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
 class ArtistRow extends React.Component {
+
+    getFlagClass(country) {
+      let clazz = null;
+      if (country) {
+        clazz = 'flag flag-' + country.toLowerCase();
+      }
+      return clazz;
+    }
+
+    getTwitterLinkIcon(url) {
+      let linkIcon = null;
+      if (url) {
+        linkIcon = (
+          <a href={url} target="_blank">
+            <i className="fab fa-twitter"></i>
+          </a>
+        );
+      }
+      return linkIcon;
+    }
+
     render() {
       const artist = this.props.artist;
-      const flagClass = artist.artist_country?  'flag flag-' + artist.artist_country.toLowerCase() : '';
-
+      const flagClass = this.getFlagClass(artist.artist_country);
+      const twitterLinkicon = this.getTwitterLinkIcon(artist.artist_twitter_url);
+      
       return (
         <tr>
           <td>{artist.artist_name}</td>
           <td><img src="blank.gif" className={flagClass} /></td>
           <td>{artist.artist_rating}</td>
-          <td>{artist.artist_twitter_url}</td>
+          <td>{twitterLinkicon}</td>
         </tr>
       );
     }
