17191133c5295d4b7dbff056c2167892c072e6ac
Márcio Gabriel <marcioggs@gmail.com>
2018-03-11 21:39:37 -0300

    Included country flags.

--- src/components/ArtistRow.js
+++ src/components/ArtistRow.js
@@ -3,11 +3,12 @@ import React from 'react';
 class ArtistRow extends React.Component {
     render() {
       const artist = this.props.artist;
-  
+      const flagClass = artist.artist_country?  'flag flag-' + artist.artist_country.toLowerCase() : '';
+
       return (
         <tr>
           <td>{artist.artist_name}</td>
-          <td>{artist.artist_country}</td>
+          <td><img src="blank.gif" className={flagClass} /></td>
           <td>{artist.artist_rating}</td>
           <td>{artist.artist_twitter_url}</td>
         </tr>
